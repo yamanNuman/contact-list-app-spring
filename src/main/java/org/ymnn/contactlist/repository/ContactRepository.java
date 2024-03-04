77c006807c22a06c43cd4e0dd7c9fb50f611bf0a
yamanNuman <yamannuman41@gmail.com>
2024-03-04 19:32:59 +0300

    contact repo updated

--- src/main/java/org/ymnn/contactlist/repository/ContactRepository.java
+++ src/main/java/org/ymnn/contactlist/repository/ContactRepository.java
@@ -3,5 +3,8 @@ package org.ymnn.contactlist.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ymnn.contactlist.entity.Contact;
 
+import java.util.Optional;
+
 public interface ContactRepository extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
 }
